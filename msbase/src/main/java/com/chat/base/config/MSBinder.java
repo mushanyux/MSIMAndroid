fc461ea8790d53425bbf2be24789b88d6810e727
mushanyu <2910714313@qq.com>
2025-07-13 23:26:44 +0800

    增加功能：api，debug，系统账户配置

---
+++ msbase/src/main/java/com/chat/base/config/MSBinder.java
@@ -0,0 +1,8 @@
+package com.chat.base.config;
+
+import com.chat.base.BuildConfig.BuildConfig;
+
+public class MSBinder {
+    //    public static boolean isDebug =  false;
+    public static boolean isDebug =  BuildConfig.DEBUG;
+}
