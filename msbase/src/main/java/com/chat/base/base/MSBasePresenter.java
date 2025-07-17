6e5d44128e381473272cb1e5cd8a2a60ab490694
mushanyu <2910714313@qq.com>
2025-07-17 22:56:32 +0800

    增加功能：基础activity和fragment

---
+++ msbase/src/main/java/com/chat/base/base/MSBasePresenter.java
@@ -0,0 +1,5 @@
+package com.chat.base.base;
+
+public interface MSBasePresenter {
+    void showLoading();
+}
---
+++ msbase/src/main/java/com/chat/base/base/MSBaseView.java
@@ -0,0 +1,7 @@
+package com.chat.base.base;
+
+public interface MSBaseView {
+    void showError(String msg);
+
+    void hideLoading();
+}
