addf0baf86c8f60cf044615dea16f5cbd209b502
mushanyu <2910714313@qq.com>
2025-07-20 19:47:20 +0800

    增加功能：滚动工具

---
+++ msbase/src/main/java/com/chat/base/views/scroll/LayoutParamsUtils.java
@@ -0,0 +1,18 @@
+package com.chat.base.views.scroll;
+
+import android.view.ViewGroup;
+
+public class LayoutParamsUtils {
+
+    /**
+     * 使子view的topMargin和bottomMargin属性无效
+     *
+     * @param params
+     */
+    public static void invalidTopAndBottomMargin(ViewGroup.MarginLayoutParams params){
+        if (params != null) {
+            params.topMargin = 0;
+            params.bottomMargin = 0;
+        }
+    }
+}
