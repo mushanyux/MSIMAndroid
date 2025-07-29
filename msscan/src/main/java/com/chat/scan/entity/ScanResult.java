26f17bcce62ca8073aa6577d69d7688437a3a130
mushanyu <2910714313@qq.com>
2025-07-29 22:57:06 +0800

    增加功能：扫描实体类

---
+++ msscan/src/main/java/com/chat/scan/entity/ScanResult.java
@@ -0,0 +1,12 @@
+package com.chat.scan.entity;
+
+import java.util.Map;
+
+/**
+ * 扫一扫结果
+ */
+public class ScanResult {
+    public String forward;
+    public String type;
+    public Map data;
+}
