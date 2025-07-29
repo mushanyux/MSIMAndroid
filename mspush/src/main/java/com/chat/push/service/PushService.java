9f511739e0469fab6c312a91d06add0cd2503abf
mushanyu <2910714313@qq.com>
2025-07-29 22:56:19 +0800

    增加功能：通用推送功能

---
+++ mspush/src/main/java/com/chat/push/service/PushService.java
@@ -0,0 +1,24 @@
+package com.chat.push.service;
+
+import com.alibaba.fastjson.JSONObject;
+import com.chat.base.net.entity.CommonResponse;
+
+import io.reactivex.rxjava3.core.Observable;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.POST;
+
+/**
+ * 推送module
+ */
+public interface PushService {
+
+    @POST("user/device_token")
+    Observable<CommonResponse> registerAppToken(@Body JSONObject jsonObject);
+
+    @DELETE("user/device_token")
+    Observable<CommonResponse> unRegisterAppToken();
+
+    @POST("user/device_badge")
+    Observable<CommonResponse> registerBadge(@Body JSONObject jsonObject);
+}
