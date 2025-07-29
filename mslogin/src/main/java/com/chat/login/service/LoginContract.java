f7f5937fbb6391c65fb021fe22f4cbdf95dca098
mushanyu <2910714313@qq.com>
2025-07-29 20:14:24 +0800

    增加功能：登录present层

---
+++ mslogin/src/main/java/com/chat/login/service/LoginContract.java
@@ -0,0 +1,56 @@
+package com.chat.login.service;
+
+
+import android.content.Context;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.chat.base.base.MSBasePresenter;
+import com.chat.base.base.MSBaseView;
+import com.chat.base.entity.UserInfoEntity;
+import com.chat.login.entity.CountryCodeEntity;
+
+import java.util.List;
+
+/**
+ * 登录
+ */
+public class LoginContract {
+    public interface LoginPersenter extends MSBasePresenter {
+        void login(String name, String pwd);
+
+        void sendLoginAuthVerifCode(String uid);
+
+        void getCountryCode();
+
+        void registerCode(String zone, String phone);
+
+        void forgetPwd(String zone, String phone);
+
+        void registerApp(String code, String zone, String name, String phone, String password,String inviteCode);
+
+        void checkLoginAuth(String uid, String code);
+
+        void resetPwd(String zone, String phone, String code, String pwd);
+    }
+
+    public interface LoginView extends MSBaseView {
+        void loginResult(UserInfoEntity userInfoEntity);
+
+        void setCountryCode(List<CountryCodeEntity> list);
+
+        void setRegisterCodeSuccess(int code, String msg, int exist);
+
+        void setLoginFail(int code, String uid, String phone);
+
+        void setSendCodeResult(int code, String msg);
+
+        void setResetPwdResult(int code, String msg);
+
+        Button getVerfiCodeBtn();
+
+        EditText getNameEt();
+
+        Context getContext();
+    }
+}
