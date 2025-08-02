7c325dc1b3846460991473fd8018541c66fb23e5
mushanyu <2910714313@qq.com>
2025-08-02 20:39:25 +0800

    增加功能：聊天事件处理

---
+++ msuikit/src/main/java/com/chat/uikit/chat/manager/ChatEventManager.java
@@ -0,0 +1,28 @@
+package com.chat.uikit.chat.manager;
+
+/**
+ * 聊天事件处理
+ */
+public class ChatEventManager {
+    private ChatEventManager() {
+    }
+
+    private static class ChatEventManagerBinder {
+        private final static ChatEventManager instance = new ChatEventManager();
+    }
+
+    public static ChatEventManager getInstance() {
+        return ChatEventManagerBinder.instance;
+    }
+
+    private boolean isShowImg = false;
+
+    public void setShowImg(boolean isShowImg) {
+        this.isShowImg = isShowImg;
+    }
+
+    public boolean getShowImg() {
+        return isShowImg;
+    }
+
+}
