bc3f345fc9c1b37d1a6fc2f2128e62486bfb5c0f
mushanyu <2910714313@qq.com>
2025-08-03 17:32:15 +0800

    增加功能：根据日期搜索

---
+++ msuikit/src/main/java/com/chat/uikit/chat/search/date/SearchWithDateEntity.java
@@ -0,0 +1,30 @@
+package com.chat.uikit.chat.search.date;
+
+import com.chad.library.adapter.base.entity.MultiItemEntity;
+
+import java.util.List;
+
+/**
+ * 通过日期搜索聊天记录
+ */
+public class SearchWithDateEntity implements MultiItemEntity {
+    public int itemType;
+    public String day;
+    public boolean selected;
+    public boolean isToDay;
+    public String date;
+    public long dayCount;
+    public long orderSeq;
+    public boolean isNull;
+    public List<SearchWithDateEntity> list;
+
+    @Override
+    public int getItemType() {
+        return itemType;
+    }
+
+    public SearchWithDateEntity(int itemType) {
+        this.itemType = itemType;
+    }
+
+}
